a192c6e32e0aca929eed0e0af453f4c339e5a516
Pritam <sealpritam@gmail.com>
2022-02-07 08:47:32 +0530

    pom

---
+++ src/main/java/com/practice/readfile/web/ReadFileApplication.java
@@ -0,0 +1,15 @@
+package com.practice.readfile.web;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication(scanBasePackages = {"com.practice"})
+@EnableTransactionManagement
+public class ReadFileApplication {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
